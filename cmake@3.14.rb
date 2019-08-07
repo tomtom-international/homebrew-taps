310b4dbf3e0b847573be45e80cce878ba2eace1a
Tomasz Maj <Tomasz.Maj@tomtom.com>
2019-08-07 13:36:52 +0200

    Add formula for CMake 3.14

---
+++ cmake@3.14.rb
@@ -0,0 +1,53 @@
+class CmakeAT314 < Formula
+  desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  url "https://github.com/Kitware/CMake/releases/download/v3.14.6/cmake-3.14.6.tar.gz"
+  sha256 "4e8ea11cabe459308671b476469eace1622e770317a15951d7b55a82ccaaccb9"
+  head "https://cmake.org/cmake.git"
+
+  depends_on "sphinx-doc" => :build
+
+  # The completions were removed because of problems with system bash
+
+  # The `with-qt` GUI option was removed due to circular dependencies if
+  # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
+  # For the GUI application please instead use `brew cask install cmake`.
+
+  def install
+    ENV.cxx11 if MacOS.version < :mavericks
+
+    # Avoid the following compiler error:
+    # SecKeychain.h:102:46: error: shift expression '(1853123693 << 8)' overflows
+    ENV.append_to_cflags "-fpermissive" if MacOS.version <= :lion
+
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build
+      --sphinx-html
+      --sphinx-man
+      --system-zlib
+      --system-bzip2
+      --system-curl
+    ]
+
+    # There is an existing issue around macOS & Python locale setting
+    # See https://bugs.python.org/issue18378#msg215215 for explanation
+    ENV["LC_ALL"] = "en_US.UTF-8"
+
+    system "./bootstrap", *args, "--", "-DCMAKE_BUILD_TYPE=Release"
+    system "make"
+    system "make", "install"
+
+    elisp.install "Auxiliary/cmake-mode.el"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+  end
+end
