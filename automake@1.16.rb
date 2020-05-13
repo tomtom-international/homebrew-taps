2a8eacb712bd0ec9ae959666dd4cd03c91001dfa
Anna Nowak <wiorka@gmail.com>
2020-05-13 21:36:27 +0200

    Add cmake 3.17; Update existing automake formula

--- automake@1.16.rb
+++ automake@1.16.rb
@@ -1,19 +1,18 @@
 class AutomakeAT116 < Formula
   desc "Tool for generating GNU Standards-compliant Makefiles"
   homepage "https://www.gnu.org/software/automake/"
-  url "https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/automake/automake-1.16.1.tar.xz"
-  sha256 "5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921"
-  revision 1
+  url "https://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/automake/automake-1.16.2.tar.xz"
+  sha256 "ccc459de3d710e066ab9e12d2f119bd164a08c9341ca24ba22c9adaa179eedd0"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fe26d4df57481b6a7ca0a6915c37c53648c27ffb41926b3570c45f80fdd8888e" => :catalina
+    sha256 "fe26d4df57481b6a7ca0a6915c37c53648c27ffb41926b3570c45f80fdd8888e" => :mojave
+    sha256 "fe26d4df57481b6a7ca0a6915c37c53648c27ffb41926b3570c45f80fdd8888e" => :high_sierra
+  end
 
   depends_on "autoconf"
-
-  # https://lists.gnu.org/archive/html/bug-automake/2018-04/msg00002.html
-  # Remove this when applying any future 1.16.2 update.
-  patch do
-    url "https://git.savannah.gnu.org/cgit/automake.git/patch/?id=a348d830659fffd2cfc42994524783b07e69b4b5"
-    sha256 "7a57ca2b91f7f3c0b168cf5ffbc8a1b2168f3886bcadcc15412281472dace3ce"
-  end
 
   def install
     ENV["PERL"] = "/usr/bin/perl"
---
+++ cmake@3.17.rb
@@ -0,0 +1,64 @@
+class CmakeAT317 < Formula
+  desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  head "https://gitlab.kitware.com/cmake/cmake.git"
+
+  stable do
+    url "https://github.com/Kitware/CMake/releases/download/v3.17.2/cmake-3.17.2.tar.gz"
+    sha256 "fc77324c4f820a09052a7785549b8035ff8d3461ded5bbd80d252ae7d1cd3aa5"
+
+    # Allows CMAKE_FIND_FRAMEWORKS to work with CMAKE_FRAMEWORK_PATH, which brew sets.
+    # Remove with 3.18.0.
+    patch do
+      url "https://gitlab.kitware.com/cmake/cmake/-/commit/c841d43d70036830c9fe16a6dbf1f28acf49d7e3.diff"
+      sha256 "87de737abaf5f8c071abc4a4ae2e9cccced6a9780f4066b32ce08a9bc5d8edd5"
+    end
+  end
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5498aed6134742450b1237e0f033ff388831a6c06e9d96524b1e14ebceef1cb1" => :catalina
+    sha256 "edc5ec271841a8b8558f8d60ef510c7d7abc91e91cc4fb0422389fad99073857" => :mojave
+    sha256 "fc6cba3364bbb477e2e4ac53a002a85976a30c437d0dfb39c528342d6d85b0fd" => :high_sierra
+  end
+
+  depends_on "sphinx-doc" => :build
+
+  # The completions were removed because of problems with system bash
+
+  # The `with-qt` GUI option was removed due to circular dependencies if
+  # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
+  # For the GUI application please instead use `brew cask install cmake`.
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build
+      --sphinx-html
+      --sphinx-man
+      --system-zlib
+      --system-bzip2
+      --system-curl
+    ]
+
+    # There is an existing issue around macOS & Python locale setting
+    # See https://bugs.python.org/issue18378#msg215215 for explanation
+    ENV["LC_ALL"] = "en_US.UTF-8"
+
+    system "./bootstrap", *args, "--", *std_cmake_args
+    system "make"
+    system "make", "install"
+
+    elisp.install "Auxiliary/cmake-mode.el"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+  end
+end
