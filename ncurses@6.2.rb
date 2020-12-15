dc8793e302384f84290356a8af75e178ce0ce987
Grzegorz Wasilewski <turbodiesel83@o2.pl>
2020-12-15 12:11:50 +0100

    feat:Add ncurses@6.2.rb

---
+++ ncurses@6.2.rb
@@ -0,0 +1,89 @@
+class NcursesAT62 < Formula
+  desc "Text-based UI library"
+  homepage "https://www.gnu.org/software/ncurses/"
+  url "https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/ncurses/ncurses-6.2.tar.gz"
+  sha256 "30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  keg_only :provided_by_macos
+
+  depends_on "pkg-config" => :build
+
+  on_linux do
+    depends_on "gpatch" => :build
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-pc-files",
+                          "--with-pkg-config-libdir=#{lib}/pkgconfig",
+                          "--enable-sigwinch",
+                          "--enable-symlinks",
+                          "--enable-widec",
+                          "--with-shared",
+                          "--with-gpm=no"
+    system "make", "install"
+    make_libncurses_symlinks
+
+    prefix.install "test"
+    (prefix/"test").install "install-sh", "config.sub", "config.guess"
+  end
+
+  def make_libncurses_symlinks
+    major = version.major
+
+    %w[form menu ncurses panel].each do |name|
+      on_macos do
+        lib.install_symlink "lib#{name}w.#{major}.dylib" => "lib#{name}.dylib"
+        lib.install_symlink "lib#{name}w.#{major}.dylib" => "lib#{name}.#{major}.dylib"
+      end
+      on_linux do
+        lib.install_symlink "lib#{name}w.so.#{major}" => "lib#{name}.so"
+        lib.install_symlink "lib#{name}w.so.#{major}" => "lib#{name}.so.#{major}"
+      end
+      lib.install_symlink "lib#{name}w.a" => "lib#{name}.a"
+      lib.install_symlink "lib#{name}w_g.a" => "lib#{name}_g.a"
+    end
+
+    lib.install_symlink "libncurses++w.a" => "libncurses++.a"
+    lib.install_symlink "libncurses.a" => "libcurses.a"
+    lib.install_symlink shared_library("libncurses") => shared_library("libcurses")
+    on_linux do
+      # libtermcap and libtinfo are provided by ncurses and have the
+      # same api. Help some older packages to find these dependencies.
+      # https://bugs.centos.org/view.php?id=11423
+      # https://bugs.launchpad.net/ubuntu/+source/ncurses/+bug/259139
+      lib.install_symlink "libncurses.so" => "libtermcap.so"
+      lib.install_symlink "libncurses.so" => "libtinfo.so"
+    end
+
+    (lib/"pkgconfig").install_symlink "ncursesw.pc" => "ncurses.pc"
+    (lib/"pkgconfig").install_symlink "formw.pc" => "form.pc"
+    (lib/"pkgconfig").install_symlink "menuw.pc" => "menu.pc"
+    (lib/"pkgconfig").install_symlink "panelw.pc" => "panel.pc"
+
+    bin.install_symlink "ncursesw#{major}-config" => "ncurses#{major}-config"
+
+    include.install_symlink [
+      "ncursesw/curses.h", "ncursesw/form.h", "ncursesw/ncurses.h",
+      "ncursesw/panel.h", "ncursesw/term.h", "ncursesw/termcap.h"
+    ]
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+
+    system prefix/"test/configure", "--prefix=#{testpath}/test",
+                                    "--with-curses-dir=#{prefix}"
+    system "make", "install"
+
+    system testpath/"test/bin/keynames"
+    system testpath/"test/bin/test_arrays"
+    system testpath/"test/bin/test_vidputs"
+  end
+end
